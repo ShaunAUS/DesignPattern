abdce4ef0bc8de9e0457048df6d3e498734eb420
ShaunAUS <wnsqud70@gmail.com>
2025-03-04 12:09:27 +0900

    init

---
+++ src/main/kotlin/com/example/designpattern/DesignpatternApplication.kt
@@ -0,0 +1,11 @@
+package com.example.designpattern
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesignpatternApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesignpatternApplication>(*args)
+}
