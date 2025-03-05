931ab9ec7a982266a1243df24f2ea53572b2e77a
ShaunAUS <wnsqud70@gmail.com>
2025-03-05 13:37:10 +0900

    feature(proxyPattern) 설명추가

--- src/main/kotlin/com/example/designpattern/proxy/ProxyRestaurant.kt
+++ src/main/kotlin/com/example/designpattern/proxy/ProxyRestaurant.kt
@@ -3,8 +3,20 @@ package com.example.designpattern.proxy
 class ProxyRestaurant(
     private val seeFoodRestaurant: SeeFoodRestaurant
 ) : Restaurant{
+
+    //main
     override fun getMenu() {
         //실제로는 다른 인터페이스 구현체 호출
         seeFoodRestaurant.getMenu()
     }
+
+    //이하 메서드들은 프록시에서 맡게되는 자잘한 메서드들
+    fun method1(){
+        println("method 1")
+    }
+
+    fun method2(){
+        println("method 2")
+    }
+
 }
